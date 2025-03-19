eb67a9940e805236b09aa0716935308584d31f0c
Avi <avipars@users.noreply.github.com>
2025-03-19 12:46:20 +0200

    documentation

--- admin_tools/battery_report.ps1
+++ admin_tools/battery_report.ps1
@@ -1,1 +1,5 @@
-powercfg /batteryreport # gives breakdown of laptop battery usage and life
+# technically it's not powershell, but you can call the program from within powershell
+
+powercfg /? # help/info regarding all available commands (some require elevation)
+
+powercfg /batteryreport # gives breakdown of laptop battery usage and life (this one doesn't need admin)
